1d5048ff8eac95e8f1256e6f903c7b3c8eb6d4a4
Vrinda Mahajan <vrindamahajan96@gmail.com>
2021-02-17 18:00:59 +0530

    added styles

--- src/components/videoItems.js
+++ src/components/videoItems.js
@@ -1,10 +1,16 @@
 import React from "react";
-import VideoList from "./videoList";
+//import VideoList from "./videoList";
+import "./videoItem.css";
 
-const VideoItems =({video}) =>{
-    return <div>
-    <img src={video.snippet.thumbnails.medium.url}></img>
-    {video.snippet.title}</div>
-}
+const VideoItems = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url}></img>
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
 
 export default VideoItems;
